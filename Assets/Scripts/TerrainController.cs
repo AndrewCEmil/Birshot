d6c7898c500d3a3592203ff5061778815ff79d00
ace <andrew.emil@gmail.com>
2016-07-09 19:31:03 -0400

    Got the terrain splatted

--- Assets/Scripts/TerrainController.cs
+++ Assets/Scripts/TerrainController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Linq;
 using System.Collections;
 
 public class TerrainController : MonoBehaviour {
@@ -41,6 +42,8 @@ public class TerrainController : MonoBehaviour {
 			//Vector3 newPos = new Vector3 (terrain.GetPosition ().x, heights[(int) terrain.GetPosition().x, (int) terrain.GetPosition().z], terrain.GetPosition ().z);
 			//Vector3 newPos = new Vector3 (100, terrain.SampleHeight(new Vector3(100, 100, 100)), 100);
 			player.transform.position = getMaxHeight();
+
+			splat ();
 		}
 	}
 
@@ -60,5 +63,71 @@ public class TerrainController : MonoBehaviour {
 			}
 		}
 		return maxPos;
+	}
+
+	//https://alastaira.wordpress.com/2013/11/14/procedural-terrain-splatmapping/
+	void splat() {
+		// Splatmap data is stored internally as a 3d array of floats, so declare a new empty array ready for your custom splatmap data:
+		float[, ,] splatmapData = new float[terrainData.alphamapWidth, terrainData.alphamapHeight, terrainData.alphamapLayers];
+
+		for (int y = 0; y < terrainData.alphamapHeight; y++) {
+			for (int x = 0; x < terrainData.alphamapWidth; x++) {
+				// Normalise x/y coordinates to range 0-1 
+				float y_01 = (float)y/(float)terrainData.alphamapHeight;
+				float x_01 = (float)x/(float)terrainData.alphamapWidth;
+
+				// Sample the height at this location (note GetHeight expects int coordinates corresponding to locations in the heightmap array)
+				float height = terrainData.GetHeight(Mathf.RoundToInt(y_01 * terrainData.heightmapHeight),Mathf.RoundToInt(x_01 * terrainData.heightmapWidth)) / terrainData.heightmapHeight;
+
+				// Calculate the normal of the terrain (note this is in normalised coordinates relative to the overall terrain dimensions)
+				Vector3 normal = terrainData.GetInterpolatedNormal(y_01,x_01);
+
+				// Calculate the steepness of the terrain
+				float steepness = terrainData.GetSteepness(y_01,x_01);
+
+				// Setup an array to record the mix of texture weights at this point
+				float[] splatWeights = new float[terrainData.alphamapLayers];
+
+				// CHANGE THE RULES BELOW TO SET THE WEIGHTS OF EACH TEXTURE ON WHATEVER RULES YOU WANT
+
+
+				// Water
+				if (height < .4) {
+					splatWeights [0] = 1;
+				} else {
+					splatWeights [0] = 0;
+				}
+
+				// Grass
+				if (height >= .4 && height < .6) {
+					splatWeights [1] = 1;
+				} else {
+					splatWeights [1] = 0;
+				}
+
+				// Ice
+				if (height >= .6) {
+					splatWeights [2] = 1;
+				} else {
+					splatWeights [2] = 0;
+				}
+
+				// Sum of all textures weights must add to 1, so calculate normalization factor from sum of weights
+				float z = splatWeights.Sum();
+
+				// Loop through each terrain texture
+				for(int i = 0; i<terrainData.alphamapLayers; i++){
+
+					// Normalize so that sum of all texture weights = 1
+					splatWeights[i] /= z;
+
+					// Assign this point to the splatmap array
+					splatmapData[x, y, i] = splatWeights[i];
+				}
+			}
+		}
+
+	// Finally assign the new splatmap to the terrainData:
+		terrainData.SetAlphamaps(0, 0, splatmapData);
 	}
 }
